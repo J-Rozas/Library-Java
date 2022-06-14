73854342a4c01199fc54dc98dc63f14b2c69cdd5
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-14 19:30:55 +0100

    Add test to check that a library has books

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
 
@@ -9,5 +12,10 @@ public class LibraryTest {
     public void before() {
         book1 = new Book("Title1", "Author1", "Genre1");
         library = new Library();
+    }
+
+    @Test
+    public void hasBooks() {
+        assertEquals(1, library.getNumberOfBooks);
     }
 }
