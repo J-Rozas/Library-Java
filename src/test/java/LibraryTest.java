6e070510c0c35e8cfb51edceace6c07ee8f0db34
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-14 21:22:18 +0100

    Add test to check that the hashmap is created correctly

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,16 +1,20 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
 
     private Library library;
     private Book book1;
+    private Book book2;
 
     @Before
     public void before() {
         book1 = new Book("Title1", "Author1", "Genre1");
+        book2 = new Book("Title2", "Author2", "Genre2");
         library = new Library("CodeClan library", 10);
     }
 
@@ -33,5 +37,19 @@ public class LibraryTest {
         library.addBook(book1);
         library.removeBook(book1);
         assertEquals(0, library.getNumberOfBooks());
+    }
+
+    @Test
+    public void shouldBeAbleToGetNumberOfBooksByGenre() {
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book2);
+        HashMap<String, Integer> expected = new HashMap<>();
+        expected.put("Genre1", 5);
+        expected.put("Genre2", 1);
+        assertEquals(expected, library.getNumberOfBooksOfEachGenre());
     }
 }
