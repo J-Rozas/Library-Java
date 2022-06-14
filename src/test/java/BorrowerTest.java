002e5f65da14ee3bc052308c7711eb942fab9cc8
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-14 20:34:29 +0100

    Add test to check that a borrower can borrow a book from a library

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -6,10 +6,15 @@ import static org.junit.Assert.assertEquals;
 public class BorrowerTest {
 
     private Borrower borrower;
+    private Library library;
+    private Book book1;
 
     @Before
     public void before() {
         borrower = new Borrower("Joe");
+        book1 = new Book("Title1", "Author1", "Genre1");
+        library = new Library("CodeClan library", 30);
+        library.addBook(book1);
     }
 
     @Test
@@ -20,5 +25,12 @@ public class BorrowerTest {
     @Test
     public void hasZeroBooksAtFirst() {
         assertEquals(0, borrower.numberOfBorrowedBooks());
+    }
+
+    @Test
+    public void shouldBeAbleToBorrowBook() {
+        borrower.borrowBook(book1);
+        assertEquals(1, borrower.numberOfBorrowedBooks());
+        assertEquals(0, library.getNumberOfBooks());
     }
 }
